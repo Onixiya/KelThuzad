2aa8029fae0f1e0ce5318ac9c59b2a326ddbaa67
silentstorm <mynutshang@cbt.com>
2022-05-14 18:00:30 +1000

    project rename

---
+++ Bigglesworth.cs
@@ -0,0 +1,30 @@
+﻿namespace KelThuzad.Pets{
+    /*[HarmonyPatch(typeof(Modding),"CheckForMods")]
+    public class Bigglesworth{
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result){
+            __result=false;
+        }
+    }
+    [HarmonyPatch(typeof(Btd6Player),"Save")]
+    public class thing{
+        public static bool ran=false;
+        [HarmonyPostfix]
+        public static void Postfix(ref Btd6Player __instance){
+            if(ran==false&&idfk.Count!=0){
+                foreach(string item in idfk){
+                    Log(item);
+                    __instance.AddTrophyStoreItem(item);
+                }
+                ran=true;
+            }
+        }
+    }*/
+    [HarmonyPatch(typeof(Pet),"Initialise")]
+    public class test{
+        [HarmonyPostfix]
+        public static void Postfix(Entity target,Model modelToUse){
+            Log(modelToUse.Cast<PetModel>().name);
+        }
+    }
+}
--- Bundles/Bundles.Designer.cs
+++ Bundles/Bundles.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WarExpansion.Assets {
+namespace KelThuzad.Bundles {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace WarExpansion.Assets {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Assets {
+    internal class Bundles {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Assets() {
+        internal Bundles() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace WarExpansion.Assets {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WarExpansion.Assets.Assets", typeof(Assets).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KelThuzad.Bundles.Bundles", typeof(Bundles).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
